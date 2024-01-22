fdff765983063d49f90d82bf8ca0f28c0665b401
daimessdn@gmail.com <daimessdn@gmail.com>
2024-01-22 07:54:01 +0700

    updated member api using query params

--- api/members.api.php
+++ api/members.api.php
@@ -4,13 +4,20 @@
 
 
 <?php
-$get_member = $conn->prepare("SELECT * FROM anggota");
+if (isset($_GET["q"])) {
+    $q = $_GET["q"];
+    $member_query = "SELECT * FROM anggota WHERE LOWER(nama) LIKE '%" . $q . "%';";
+} else {
+    $member_query = "SELECT * FROM anggota;";
+}
+
+$get_member = $conn->prepare($member_query);
 $get_member->execute();
 
 $member_json = json_encode([
     "success" => true,
     "msg" => "berhasil menampilkan data anggota",
-    "data" => $get_member->fetch(PDO::FETCH_ASSOC)
+    "data" => $get_member->fetchAll(PDO::FETCH_ASSOC)
 ]);
 
 echo $member_json;
